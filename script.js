df6919cbca036ebb8fc540a2a2b053e79aa28a86
0XZxX0 <jduloon@gmail.com>
2026-06-30 00:51:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+Gutta World Website - Updated FiveManage Video URL
+
+Upload ALL files/folders in this folder to your GitHub repo root:
+index.html
+style.css
+script.js
+config.js
+images/logo.png
+
+No videos folder is needed now.
+Background video is loaded from:
+https://r2.fivemanage.com/DO9KoRL23hOK71rF7NFU8/lovesosa.mp4
+
+To change countdown, edit config.js.
