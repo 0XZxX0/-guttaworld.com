278f2c4b8f861ff11058020f5827ec0dc5bbba71
0XZxX0 <jduloon@gmail.com>
2026-07-06 13:46:44 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@
 // Add more MP4 music/video links inside videoPlaylist.
 // Put a comma after every link except the last one.
 window.GUTTA_CONFIG = {
-  launchDate: "2026-07-30T20:00:00-05:00",
+  launchDate: "2026-07-15T20:00:00-05:00",
 
   videoPlaylist: [
     "https://r2.fivemanage.com/DO9KoRL23hOK71rF7NFU8/lovesosa.mp4",
     "https://r2.fivemanage.com/DO9KoRL23hOK71rF7NFU8/rollout.mp4",
     "https://r2.fivemanage.com/DO9KoRL23hOK71rF7NFU8/LongLiveJefferyDahmer(OfficialVideo)ft.LilScoom89BloodyhoundLilJeff-Ybcdul(1080p).mp4",
     "https://r2.fivemanage.com/DO9KoRL23hOK71rF7NFU8/Dragnutz-Out6ix-Dragnutz(1080p)(1).mp4",
-    "https://r2.fivemanage.com/DO9KoRL23hOK71rF7NFU8/YoungPappy-Killa(OfficialMusicVideo)-TFGEnt(800p)(1).mp4",
+    "https://r2.fivemanage.com/DO9KoRL23hOK71rF7NFU8/YoungPappy-Killa(OfficialMusicVideo)-TFGEnt(800p)(1).mp4"
   ],
 
   randomStart: true,
